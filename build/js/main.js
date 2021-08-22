402ac110e9b89a3b474b26884d0d37fd9aa839e9
daniilBarabanov <flame146@mail.ru>
2021-08-22 12:59:15 +0300

    test2

--- build/js/main.js
+++ build/js/main.js
@@ -106,3 +106,30 @@
     }
   });
 })();
+
+const tabItems = document.querySelectorAll('.months-item__link');
+const contentItems = document.querySelectorAll('.subscription-cards');
+
+const findClearActiveClass = (elements, className = 'is-active')  => {
+    Array.from(elements).find(item =>
+        item.classList.remove(`${className }`))
+}
+
+const setActiveClass = (element, index, className = 'is-active') => {
+    element[index].classList.add(`${ className}`)
+}
+
+const checkoutTabs = (item, index) => {
+    item.addEventListener('click', () => {
+        if (item.classList.contains('is-active')) return
+
+        const currentItem = index
+        
+        findClearActiveClass(tabItems)
+        findClearActiveClass(contentItems)
+
+        setActiveClass(tabItems, currentItem)
+        setActiveClass(contentItems, currentItem)
+    })
+}
+tabItems.forEach(checkoutTabs)
--- build/js/vendor.js
+++ build/js/vendor.js
@@ -4,24 +4,3 @@
   $('.questions-form__mask').mask('+7(999)999-99-99');
   $('.form__popup__mask').mask('+7(999)999-99-99');
 })();
-
-/*eslint-disable*/
-
-(function() {
-  function resizeBlock(){
-    if(window.matchMedia("screen and (max-width: 1023px)").matches) {
-       $('.footer-top__left').append($('#1999'));
-     }
-     else if (window.matchMedia("screen and (min-width: 1023px)").matches){
-       $('#1999').appendTo($('#copy'));
-     }
-   }
-   
-   $(window).resize(function() {
-        resizeBlock();
-   });
-   
-   $(document).ready(function(){
-       resizeBlock();
-    });
-})();
